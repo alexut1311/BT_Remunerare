ee2d859f634b1f49edb60d4566a973695c53381a
Alexandru Serban <alexut1311@gmail.com>
2023-01-19 19:21:31 +0200

    Ran cleanup code

--- BT_Remunerare.Tests/ControllerTests/PeriodControllerTests.cs
+++ BT_Remunerare.Tests/ControllerTests/PeriodControllerTests.cs
@@ -1,7 +1,5 @@
-﻿using BT_Remunerare.BL.Classes;
-using BT_Remunerare.BL.Interfaces;
+﻿using BT_Remunerare.BL.Interfaces;
 using BT_Remunerare.Controllers;
-using BT_Remunerare.Helpers.Classes;
 using BT_Remunerare.Helpers.Interfaces;
 using BT_Remunerare.Models;
 using BT_Remunerare.TL.Common;
@@ -9,19 +7,14 @@ using BT_Remunerare.TL.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BT_Remunerare.Tests.ControllerTests
 {
     public class PeriodControllerTests
     {
-        private Mock<IPeriodLogic> _periodLogic;
-        private Mock<IPeriodControllerHelper<PeriodViewModel, PeriodDTO>> _periodControllerHelper;
-        private PeriodController _periodController;
+        private Mock<IPeriodLogic>? _periodLogic;
+        private Mock<IPeriodControllerHelper<PeriodViewModel, PeriodDTO>>? _periodControllerHelper;
+        private PeriodController? _periodController;
 
 
         [SetUp]
@@ -33,26 +26,26 @@ namespace BT_Remunerare.Tests.ControllerTests
         }
 
         [Test]
-        public async Task AddPeriod_IsExecuted()
+        public void AddPeriod_IsExecuted()
         {
-            var response = new Response
+            Response response = new()
             {
                 IsSuccesful = true
             };
 
-            var periodDTO = new PeriodDTO
+            PeriodDTO periodDTO = new()
             {
                 Year = 1,
                 Month = 1
             };
 
-            _periodControllerHelper.Setup(s => s.BuildDTO(It.IsAny<PeriodViewModel>())).Returns(periodDTO);
-            _periodLogic.Setup(s => s.AddPeriod(It.IsAny<PeriodDTO>())).Returns(response);
+            _ = _periodControllerHelper.Setup(s => s.BuildDTO(It.IsAny<PeriodViewModel>())).Returns(periodDTO);
+            _ = _periodLogic.Setup(s => s.AddPeriod(It.IsAny<PeriodDTO>())).Returns(response);
 
             //Act
-            StatusCodeResult result = _periodController.AddPeriod(It.IsAny<PeriodViewModel>()) as StatusCodeResult;
+            StatusCodeResult? result = _periodController.AddPeriod(It.IsAny<PeriodViewModel>()) as StatusCodeResult;
             Assert.That(result.StatusCode, Is.EqualTo(200));
-            
+
             //Assert
             //result.IsSuccesful.Should().Be(result.IsSuccesful);
             //saleRepository.Verify(s => s.AddSale(It.IsAny<SaleDTO>()), Times.Once);
